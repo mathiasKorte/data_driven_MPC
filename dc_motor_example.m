cae3b5f2082fa20bfb6fc94d169dfa862a8fade3
Mathias Korte <mathias.korte@tum.de>
2022-07-18 10:04:28 +0200

    delete unused files, rename files

--- dc_motor_example.m
+++ dc_motor_example.m
@@ -1,70 +1,95 @@
 %% Prepare workspace
-close;
-clear;
+close all;
+clear all;
 clc;
 
+%% Create the state-space representatnion
+noise = 0.01;
 
-%% Constructing a State-Space Model of the DC Motor
-% Enter the values to specify the DC motor
-R= 0.4;     % Ohms
-L= 0.5;     % Henrys
-Km = .015;  % torque constant
-Kb = .015;  % emf constant
-Kf = 0.2;   % Nms
-J= 0.2;    % kg.m^2
+% State-space system matrices
+A=[-1.2,0.03;0.075,-1];
+B=[2;0];
+C=[0,1];
+D=0;
 
-% Given the values of the DC motor, we can create the state-space
-% representatnion
-A = [-R/L -Kb/L; Km/J -Kf/J];
-B = [1/L; 0];
-C = [0 1];
-D = [0];
+% Matrix dimensions
+x_dim = 2;
+y_dim = 1;
+u_dim = 1;
 
-ts = 0.05;  % System sample time
-sys_dc = ss(A,B,C,D,ts) % Create the discrete system
+%% Generate input and output trajectories
+u_d = [];
+y_d = [];
 
-% Generate a input trajectory
-t = 0:ts:8;  
-u_d = randn(length(t),1);   % Generate a random one dimensional input
-save('DC_motor_input.mat','u_d');
-%u_d = load('DC_motor_input.mat').u_d;    % Load predefinded input for testing
+% Generate random start state
+x = randn(x_dim,1);
 
-%% Plot the system response.
-figure()
-lsim(sys_dc,u_d,t)
-legend('System')
-grid on
+for i=1:50
+    u = randn(u_dim,1);
+    y = C * x + D * u + randn(y_dim,1)*noise;
+    x = A * x + B * u;
+    u_d = [u_d; u'];
+    y_d = [y_d; y'];
+end
 
-x0 = [1 2];
-y_d = lsim(sys_dc,u_d,t,x0);   % Simulate system response
+%% Open-loop plots
+
+figure
+subplot(2,1,1)
+plot(y_d)
+grid on;
+title('Y');
+subplot(2,1,2)
+plot(u_d)
+grid on;
+title('U')
 
 %% Initialization of DDMPC
-n = 2;      % Upper system order
-L = 5;     % Prediction horizon
-R = 0.1;    % Cost matrix R
-Q = 1;      % Cost matix Q
+n = x_dim;      % Upper bound on system order   
+L = 10;         % Prediction horizon
+R = 0;          % Cost matrix R
+Q = eye(y_dim); % Cost matrix Q
 
-ddmpc= DDMPC(u_d,y_d,Q,R,2,L);
+ddmpc= DDMPC(u_d,y_d,Q,R,n,L,'y_s',0,....
+    'G_mat_u',[1;-1],'g_vec_u',[1;1]*5,....
+    ...%'G_mat_y',[1,0;-1,0;0,1;0,-1],'g_vec_y',ones(4,1)*30,....
+    'lambda_sigma',100, 'ctrl_mode', 'robust');
+    
+u = randn(u_dim,1)*0;
+x = randn(x_dim,1)*10;
 
-%% Execution of DDMPC
-% Initial training values
-u = randn(1,1);
-x = randn(n,1);
+%% Run DDMPC
 
 u_traj = [];
 y_traj =[];
-for i=1:100
+for i=1:30
     y = C * x + D * u;
     x = A * x + B * u;
+
+     y = y+randn(y_dim,1)*noise;
+
     u = ddmpc.step(u,y);
     u_traj = [u_traj; u'];
     y_traj = [y_traj; y'];
 end
 
-% Plot training
-figure
-plot(y_traj)
+%% Closed-loop plots
+% Plot closed-loop input
+figure()
 hold on;
-plot(u_traj)
+title('');
+xlabel('Time t');
+ylabel('Closed-loop input u_{app} [V]'); 
+plot(u_traj,'DisplayName','Input u_1');   % Trajectory of input 1
 grid on;
-legend('Output','Input');
+legend('Location','southeast');
+
+% Plot closed-loop output
+figure
+hold on;
+title('');
+xlabel('Time t');
+ylabel('Closed-loop output \omega [s^{-1}]');
+plot(y_traj,'DisplayName','Output y_1');   % Trajectory of tank level 1
+grid on;
+legend('Location','southeast');
